06f82966409f5d133e4acab33d268c95fb4185c5
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-20 17:15:27 +0200

    Fix import error

--- src/utils/ColorScheme.js
+++ src/utils/ColorScheme.js
@@ -6,7 +6,7 @@
 * LICENSE file in the root directory of this source tree.
 */
 
-import schemes from 'msa-colorschemes';
+import schemes from '../colorschemes';
 const schemesMgr = new schemes();
 
 /**
