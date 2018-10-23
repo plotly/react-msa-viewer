343b049ede90d97525cad8b1e33f4955627e18ea
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-23 17:51:39 +0200

    Listen to the special positionStore in xBar

--- src/components/xBar.js
+++ src/components/xBar.js
@@ -5,11 +5,36 @@
 * This source code is licensed under the MIT license found in the
 * LICENSE file in the root directory of this source tree.
 */
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import createRef from 'create-react-ref/lib/createRef';
 import createShallowCompare from '../utils/createShallowCompare';
+
+class ListComponent extends PureComponent {
+
+  componentWillUpdate() {
+    console.log("CWU");
+  }
+
+  render() {
+    const TileComponent = this.props.tileComponent;
+    const elements = [];
+    let xPos = 0;
+    for (let i = this.props.startTile; i < this.props.endTile; i++) {
+      elements.push(
+        <TileComponent
+          key={i}
+          index={i}
+          />
+      );
+      xPos += this.props.tileWidth;
+      if (xPos > this.props.maxWidth)
+          break;
+    }
+    return elements;
+  }
+}
 
 /**
 * Displays the sequence names with an arbitrary Marker component
@@ -32,45 +57,44 @@ class XBarComponent extends Component {
       if (shallowCompare(this.props, nextProps)) {
         return true;
       }
-      if (Math.abs(nextProps.currentViewSequencePosition - this.lastCurrentViewSequencePosition) >= this.props.cacheElements) {
-        return true;
-      }
-      return this.updateScrollPosition();
+      return this.shouldRerender();
     };
-  }
-
-  draw() {
-    const TileComponent = this.props.tileComponent;
-    const elements = [];
-    let xPos = this.props.xPosOffset;
-    const startTile = Math.max(0, this.props.currentViewSequencePosition - this.props.cacheElements);
-    const endTile = Math.min(this.props.maxLength, startTile + this.props.nrTiles + this.props.cacheElements * 2);
-    for (let i = startTile; i < endTile; i++) {
-      elements.push(
-        <TileComponent
-          key={i}
-          index={i}
-          />
-      );
-      xPos += this.props.tileWidth;
-      if (xPos > (this.props.width + this.props.cacheElements * 2 * this.props.tileWidth))
-          break;
-    }
-    this.lastCurrentViewSequencePosition = this.props.currentViewSequencePosition;
-    this.lastStartTile = startTile;
-    return elements;
   }
 
   componentDidUpdate() {
     this.updateScrollPosition();
   }
 
+  shouldRerender() {
+      if (Math.abs(this.currentViewSequencePosition - this.lastCurrentViewSequencePosition) >= this.props.cacheElements) {
+        return true;
+      }
+      return this.updateScrollPosition();
+  }
+
+  updateFromPositionStore = () => {
+    const state = this.context.positionMSAStore.getState();
+    this.xPosOffset = state.xPosOffset;
+    this.currentViewSequencePosition = state.currentViewSequencePosition;
+    console.log(this.xPosOffset);
+    if (this.shouldRerender()) {
+      this.setState({
+        xPosOffset: this.xPosOffset,
+        currentViewSequencePosition: this.currentViewSequencePosition,
+      });
+    }
+  }
+
+  componentWillMount() {
+    this.updateFromPositionStore();
+    this.context.positionMSAStore.subscribe(this.updateFromPositionStore);
+  }
   updateScrollPosition() {
     if (this.el.current) {
-      let offset = -this.props.xPosOffset;
+      let offset = -this.xPosOffset;
       offset += (this.lastCurrentViewSequencePosition - this.lastStartTile) * this.props.tileWidth;
-      if (this.props.currentViewSequencePosition !== this.lastCurrentViewSequencePosition) {
-        offset += (this.props.currentViewSequencePosition - this.lastCurrentViewSequencePosition) * this.props.tileWidth;
+      if (this.currentViewSequencePosition !== this.lastCurrentViewSequencePosition) {
+        offset += (this.currentViewSequencePosition - this.lastCurrentViewSequencePosition) * this.props.tileWidth;
       }
       this.el.current.scrollLeft = offset;
     }
@@ -79,9 +103,7 @@ class XBarComponent extends Component {
 
   render() {
     const {
-      xPosOffset,
       tileWidth,
-      currentViewSequencePosition,
       sequences,
       width,
       cacheElements,
@@ -100,14 +122,33 @@ class XBarComponent extends Component {
       ...this.props.style,
       height: this.props.height,
     };
+    const forwardedProps = {
+      tileWidth,
+      sequences,
+      tileComponent,
+    };
+    this.lastCurrentViewSequencePosition = this.currentViewSequencePosition;
+    const startTile = Math.max(0, this.currentViewSequencePosition - this.props.cacheElements);
+    const endTile = Math.min(this.props.maxLength, startTile + this.props.nrTiles + this.props.cacheElements * 2);
+    const maxWidth = this.props.width + this.props.cacheElements * 2 * this.props.tileWidth;
+    this.lastStartTile = startTile;
+    console.log(startTile, endTile);
     return (
       <div style={containerStyle} {...otherProps}>
         <div style={style} ref={this.el}>
-          { this.draw() }
+          <ListComponent {...forwardedProps}
+            startTile={startTile}
+            endTile={endTile}
+            maxWidth={maxWidth}
+          />
         </div>
       </div>
     );
   }
+}
+
+XBarComponent.contextTypes = {
+  positionMSAStore: PropTypes.object,
 }
 
 XBarComponent.propTypes = {
@@ -122,9 +163,9 @@ XBarComponent.propTypes = {
   cacheElements: PropTypes.number.isRequired,
 
   tileWidth: PropTypes.number.isRequired,
-  currentViewSequencePosition: PropTypes.number.isRequired,
+  //currentViewSequencePosition: PropTypes.number.isRequired,
   nrTiles: PropTypes.number.isRequired,
-  xPosOffset: PropTypes.number.isRequired,
+  //xPosOffset: PropTypes.number.isRequired,
   maxLength: PropTypes.number.isRequired,
 }
 
