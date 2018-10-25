79b05508e93f70cae7a20819fc58e5767b109337
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-25 20:35:08 +0200

    Put shallowEqual in the source tree for ES6 module compatibility

--- src/components/Canvas/CanvasCache.js
+++ src/components/Canvas/CanvasCache.js
@@ -6,7 +6,7 @@
 * LICENSE file in the root directory of this source tree.
 */
 
-import * as shallowEqual from 'shallowequal';
+import shallowEqual from '../../utils/shallowEqual';
 import {
   minBy
 } from 'lodash-es';
