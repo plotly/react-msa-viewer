bff1f3459da0e0f4cef937388607fb53d8c3cd10
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-25 20:15:13 +0200

    Limit the maxSize of the CanvasCache and auto-remove old elements

--- src/components/Canvas/CanvasCache.js
+++ src/components/Canvas/CanvasCache.js
@@ -7,14 +7,18 @@
 */
 
 import * as shallowEqual from 'shallowequal';
+import {
+  minBy
+} from 'lodash-es';
 
 /**
  * A simple, in-memory cache for Canvas tiles.
  * Gets automatically invalidated when called with different widths.
  */
 class CanvasCache {
-  constructor() {
-    this.cache = {};
+  constructor({maxElements} = {}) {
+    this.maxElements = maxElements || 200;
+    this.invalidate();
   }
 
   // creates a canvas with a single letter
@@ -22,12 +26,22 @@ class CanvasCache {
   createTile({key, tileWidth, tileHeight, create}) {
     // check if cache needs to be regenerated
     if (key in this.cache) {
-      return this.cache[key];
+      return this.cache[key].value;
     }
-    const canvas = this.cache[key] = document.createElement("canvas");
+    if (this.cachedElements >= this.maxElements) {
+      // purge oldest key from cache if maxSize is reached
+      const oldestKey = minBy(Object.keys(this.cache), k => this.cache[k].insertionTime);
+      delete this.cache[oldestKey];
+    }
+    const canvas = document.createElement("canvas");
+    this.cache[key] = {
+      value: canvas,
+      insertionTime: Date.now(),
+    };
     canvas.width = tileWidth;
     canvas.height = tileHeight;
     const ctx = canvas.getContext('2d');
+    this.cachedElements++;
 
     create({canvas: ctx});
     return canvas;
@@ -40,7 +54,6 @@ class CanvasCache {
    */
   updateTileSpecs(spec) {
     if (!shallowEqual(spec, this.spec)) {
-      console.log("invalidate", spec, this.spec);
       this.invalidate();
       this.spec = spec;
       return true;
@@ -52,6 +65,7 @@ class CanvasCache {
     // TODO: destroy the old canvas elements
     this.cache = {};
     this.spec = {};
+    this.cachedElements = 0;
   }
 }
 
