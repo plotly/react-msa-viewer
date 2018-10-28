bfcb77869a331cb822aa29e61669fed3e89655c1
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-28 21:43:47 +0100

    test sending store updates on mousemove between mousedown/mouseup

--- src/components/Canvas/SequenceViewer.test.js
+++ src/components/Canvas/SequenceViewer.test.js
@@ -26,19 +26,23 @@ it('renders without crashing', () => {
 });
 
 describe('sends movement actions on mousemove events', () => {
-  const msa = mount(<MSAViewer sequences={[...dummySequences]}
-    width={400} height={200}
-    >
-    <SequenceViewer />
-  </MSAViewer>);
-  const el = msa.find('canvas').first();
+  let msa;
+  beforeEach(() =>
+    msa = mount(<FakePositionStore sequences={[...dummySequences]}
+      width={400} height={200}
+      >
+      <SequenceViewer />
+    </FakePositionStore>)
+  );
   it('should have correctly rendered the canvas', () => {
+    const el = msa.find('canvas').first();
     const props = el.props();
     expect(props.width).toBe(400);
     expect(props.height).toBe(140); // only 7 dummy sequences
   });
 
   it('should have correctly rendered the parent div', () => {
+    const el = msa.find('canvas').first();
     const props = el.parent().props();
     expect(props.width).toBe(400);
     expect(props.height).toBe(140); // only 7 dummy sequences
@@ -52,6 +56,33 @@ describe('sends movement actions on mousemove events', () => {
     expect(sv.state().mouse.cursorState).toBe('grabbing');
     sv.instance().onMouseUp({});
     expect(sv.state().mouse.cursorState).toBe('grab');
+  })
+
+  it("should send store updates on mousemove between mousedown/mouseup", () => {
+    const sv = msa.find(CanvasSequenceViewer).instance();
+    const ps = msa.instance().positionStore;
+    sv.onMouseMove({pageX: 20, pageY: 10});
+    // shouldn't send updates here
+    expect(ps.actions).toEqual([]);
+
+    sv.onMouseDown({
+      pageX: 20,
+      pageY: 10,
+    });
+    expect(ps.actions).toEqual([]);
+
+    sv.onMouseMove({
+      pageX: 40,
+      pageY: 20,
+    });
+    // should send updates here, but we need to wait for requestAnimationFrame
+    jest.runAllTimers();
+    const expected = [{"payload": {"xMovement": -20, "yMovement": -10}, "type": "POSITION_MOVE"}];
+    expect(ps.actions).toEqual(expected);
+
+    sv.onMouseUp({});
+    // shouldn't send updates here
+    expect(ps.actions).toEqual(expected);
   })
 })
 
