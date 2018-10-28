d5f08887b546ff970e1b1514472ca4f549f6c7e1
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-28 18:41:52 +0100

    Add more tests for the OverviewBar

--- src/components/xBars/OverviewBar.test.js
+++ src/components/xBars/OverviewBar.test.js
@@ -15,9 +15,9 @@ import {
 
 import { OverviewBar } from './OverviewBar';
 
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
-it('renders properly', () => {
+it('renders properly (full render)', () => {
   const component = mount(
     <FakePositionStore currentViewSequencePosition={0}>
       <OverviewBar nrXTiles={5} tileWidth={20} width={200} maxLength={100} sequences={[...dummySequences]} />
@@ -29,7 +29,35 @@ it('renders properly', () => {
 it('renders properly with a moved viewport', () => {
   let subscribee;
   const subscribeHandler = s => subscribee = s;
-  mount(<FakePositionStore currentViewSequencePosition={40} subscribe={subscribeHandler}>
-      <OverviewBar nrXTiles={5} tileWidth={20} width={200} maxLength={100} sequences={[...dummySequences]} />
+  const msa = mount(<FakePositionStore
+    currentViewSequencePosition={40} subscribe={subscribeHandler}
+    xPosOffset={0}>
+    <OverviewBar nrXTiles={5} tileWidth={20} width={100}
+      maxLength={100} sequences={[...dummySequences]} cacheElements={5} />
     </FakePositionStore>);
+  expect(msa).toMatchSnapshot();
+  const bar = msa.find(OverviewBar);
+  const xBarDiv = bar.children().first().instance().el.current;
+  expect(xBarDiv.scrollLeft).toBe(0); // fresh render
+  // call store subscribees
+  msa.setProps({currentViewSequencePosition: 10});
+  subscribee();
+  // view should have moved
+  expect(msa).toMatchSnapshot();
+  expect(xBarDiv.scrollLeft).toBe(100);
+});
+
+it('renders properly with a moved viewport', () => {
+  const msa = shallow(<OverviewBar
+      nrXTiles={5} tileWidth={20} width={100}
+      maxLength={100} sequences={[...dummySequences]} cacheElements={5} />);
+  const positionMSAStore = {
+    getState: () => ({
+      currentViewSequencePosition: 40,
+      xPosOffset: 0,
+    }),
+    subscribe: () => {},
+  };
+  const wrapper = msa.dive({context: {positionMSAStore}});
+  expect(wrapper).toMatchSnapshot();
 });
