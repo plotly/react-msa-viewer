5c80e84da5d4877238e10bf8998a7b6ec6b448fe
Sebastian Wilzbach <seb@wilzba.ch>
2018-10-28 22:42:03 +0100

    Auto-inject the MSAPositionStore

--- src/store/connect.js
+++ src/store/connect.js
@@ -7,12 +7,35 @@
 */
 
 import { connect } from 'react-redux';
+import createRef from 'create-react-ref/lib/createRef';
 
 import { storeKey } from './storeOptions';
 
+import React, { PureComponent } from 'react';
+
+// just inject the position store and further magic
+function withPositionConsumer(Component) {
+  class MSAPositionConsumer extends PureComponent {
+    constructor(props) {
+      super(props);
+      this.el = createRef();
+    }
+    render() {
+      return <Component ref={this.el} {...this.props} />
+    }
+  }
+  MSAPositionConsumer.displayName = Component.name + '-PositionStore';
+  return MSAPositionConsumer;
+}
+
 function msaConnect(mapStateToProps, mapDispatchToProps, mergeProps, options = {}) {
   options.storeKey = storeKey;
-  return connect(mapStateToProps, mapDispatchToProps, mergeProps, options);
+  const reduxConnect = connect(mapStateToProps, mapDispatchToProps, mergeProps, options);
+  return function(Component) {
+    //console.log(Component);
+    const wrappedComponent = withPositionConsumer(Component);
+    return reduxConnect(wrappedComponent);
+  }
 }
 
 export default msaConnect;
